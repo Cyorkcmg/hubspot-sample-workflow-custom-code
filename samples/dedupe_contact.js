58a91246d657bc31e0794a4e5293d24be5d5d233
Cyorkcmg <chris.york@cmg.com>
2025-06-18 14:34:06 -0400

    Update dedupe_contact.js
    adapting the file to look at mobile phone as well. It will also compare phone to mobile phone to make sure we grab as many dups as possible on phone number

--- samples/dedupe_contact.js
+++ samples/dedupe_contact.js
@@ -15,14 +15,19 @@ exports.main = (event, callback) => {
 
   // [CA-SECTION] Retrieve contact details
   hubspotClient.crm.contacts.basicApi
-    .getById(event.object.objectId, [DEDUPE_PROPERTY, 'address', 'city', 'state', 'zip'])
+    .getById(event.object.objectId, ['phone', 'mobilephone', 'address', 'city', 'state', 'zip'])
     .then(contactResult => {
       if (!contactResult || !contactResult.body || !contactResult.body.properties) {
         console.error('[CA] Could not retrieve contact properties');
         return;
       }
       // [CA-SECTION] Normalize phone and address for deduplication
-      let rawPhone = contactResult.body.properties[DEDUPE_PROPERTY] || "";
+      let rawPhone = contactResult.body.properties['phone'] || contactResult.body.properties['mobilephone'] || "";
+      if (contactResult.body.properties['phone']) {
+        console.log('[CA] Using phone for deduplication');
+      } else if (contactResult.body.properties['mobilephone']) {
+        console.log('[CA] Using mobilephone for deduplication');
+      }
       let digitsOnly = rawPhone.replace(/\D/g, '');
       // [CA-NOTE] Remove leading '1' from 11-digit US phone numbers to normalize to 10 digits.
       if (digitsOnly.length === 11 && digitsOnly.startsWith('1')) {
@@ -60,15 +65,42 @@ exports.main = (event, callback) => {
 
       // [CA-SECTION] Search for duplicates and merge if found
       console.log(`Looking for duplicates based on ${dedupeField} = ${dedupePropValue}`);
-      hubspotClient.crm.contacts.searchApi
-        .doSearch({
-          filterGroups: [{
-            filters: [{
+
+      let searchFilters = [];
+
+      if (dedupeField === 'phone') {
+        searchFilters = [{
+          filters: [
+            {
+              propertyName: 'phone',
+              operator: 'EQ',
+              value: dedupePropValue
+            }
+          ]
+        }, {
+          filters: [
+            {
+              propertyName: 'mobilephone',
+              operator: 'EQ',
+              value: dedupePropValue
+            }
+          ]
+        }];
+      } else {
+        searchFilters = [{
+          filters: [
+            {
               propertyName: dedupeField,
               operator: 'EQ',
               value: dedupePropValue
-            }]
-          }]
+            }
+          ]
+        }];
+      }
+
+      hubspotClient.crm.contacts.searchApi
+        .doSearch({
+          filterGroups: searchFilters
         })
         .then(searchResults => {
           let results = searchResults?.body?.results || [];
